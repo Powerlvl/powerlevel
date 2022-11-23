a0816c140517bdf7a09f077529b9c42dd37eebd7
Jimmie Van Eijsden <jimmie.van.eijsden@gmail.com>
2022-11-23 22:45:29 +0100

    updated website with threejs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EngineComponent } from './engine/engine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EngineComponent
   ],
   imports: [
     BrowserModule,
